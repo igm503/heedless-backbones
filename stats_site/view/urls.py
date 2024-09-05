fde8172ce7d051d45bc150f9833ce4c92044d313
igm503 <igm503@gmail.com>
2024-09-05 01:59:32 -0500

    put css in static; beginning work on family page

--- stats_site/view/urls.py
+++ stats_site/view/urls.py
@@ -4,8 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.plot_view, name='results'),
-    # path('get_plot_data/', views.get_plot_data, name='get_plot_data'),
-    path("<str:family>/", views.show_family, name="backbone_family"),
-    path("<str:dataset>/", views.show_dataset, name="dataset"),
-    path("<str:downstream_head>/", views.show_downstream_head, name="downstream_head"),
+    path("<str:family_name>/", views.show_family, name="backbone_family"),
+    path("<str:dataset_name>/", views.show_dataset, name="dataset"),
+    path("<str:downstream_head_name>/", views.show_downstream_head, name="downstream_head"),
 ]
