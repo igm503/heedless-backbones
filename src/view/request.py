b285450b40bbfd007ffbab56401bb77e52fe387a
igm503 <igm503@gmail.com>
2024-09-23 01:31:49 -0500

    refactor of request argument handler

--- src/view/request.py
+++ src/view/request.py
@@ -1,12 +1,7 @@
 from dataclasses import dataclass
 
 from .data_utils import get_axis_title
-from .models import (
-    Dataset,
-    DownstreamHead,
-    Task,
-    TaskType,
-)
+from .models import Dataset, DownstreamHead
 
 
 class PlotRequest:
@@ -17,15 +12,16 @@ class PlotRequest:
     @dataclass
     class DataArgs:
         dataset: Dataset | None = None
-        task: Task | None = None
+        task: str | None = None
         head: DownstreamHead | None = None
+        resolution: int | None = None
         gpu: str | None = None
         precision: str | None = None
         fps: bool = False
-        resolution: int | None = None
 
         def __post_init__(self):
             self.fps = self.gpu is not None and self.precision is not None
+            self.resolution = int(self.resolution) if self.resolution else None
 
     @dataclass
     class PlotArgs:
@@ -49,134 +45,51 @@ class PlotRequest:
                 self.group_by_second = None
 
     def __init__(self, args):
-        self.y_type = args["y_axis"]
-        self.x_type = args["x_axis"]
-
-        self.x_dataset = args.get("x_dataset")
-        self.x_task = args.get("x_task")
-        self.x_metric = args.get("x_metric")
-        self.x_head = args.get("x_head")
-        self.x_gpu = args.get("x_gpu")
-        self.x_precision = args.get("x_precision")
-        self.x_resolution = args.get("x_resolution")
-        self.x_resolution = int(self.x_resolution) if self.x_resolution else None
-
-        self.y_dataset = args.get("y_dataset")
-        self.y_task = args.get("y_task")
-        self.y_metric = args.get("y_metric")
-        self.y_head = args.get("y_head")
-        self.y_gpu = args.get("y_gpu")
-        self.y_precision = args.get("y_precision")
-        self.y_resolution = args.get("y_resolution")
-        self.y_resolution = int(self.y_resolution) if self.y_resolution else None
-
         self.pretrain_dataset = args.get("_pretrain_dataset")
         self.pretrain_method = args.get("_pretrain_method")
 
-        self.query_type = None
-        self.data_args = None
-        self.plot_args = None
-        self.x_args = None
-        self.y_args = None
+        x_args = PlotRequest.DataArgs(
+            dataset=args.get("x_dataset"),
+            task=args.get("x_task"),
+            head=args.get("x_head"),
+            resolution=args.get("x_resolution"),
+            gpu=args.get("x_gpu"),
+            precision=args.get("x_precision"),
+        )
+        y_args = PlotRequest.DataArgs(
+            dataset=args.get("y_dataset"),
+            task=args.get("y_task"),
+            head=args.get("y_head"),
+            resolution=args.get("y_resolution"),
+            gpu=args.get("y_gpu"),
+            precision=args.get("y_precision"),
+        )
+        self.data_args = PlotRequest.DataArgs(
+            dataset=x_args.dataset or y_args.dataset,
+            task=x_args.task or y_args.task,
+            head=x_args.head or y_args.head,
+            resolution=x_args.resolution or y_args.resolution,
+            gpu=x_args.gpu or y_args.gpu,
+            precision=x_args.precision or y_args.precision,
+        )
+        self.plot_args = PlotRequest.PlotArgs(
+            x_attr=args.get("x_metric") or args.get("x_axis"),
+            y_attr=args.get("y_metric") or args.get("y_axis"),
+            dataset=y_args.dataset or x_args.dataset,
+            x_dataset=x_args.dataset,
+            y_dataset=y_args.dataset,
+            x_task=x_args.task,
+            y_task=y_args.task,
+            group_by=args.get("legend_attribute"),
+            group_by_second=args.get("legend_attribute_(second)"),
+        )
 
-        if self.y_type == "results" and self.x_type == "results":
-            if (
-                self.x_dataset == self.y_dataset
-                and self.x_task == self.y_task
-                and self.x_head == self.y_head
-                and self.x_resolution == self.y_resolution
-            ):
+        if x_args.dataset and y_args.dataset and x_args.dataset != y_args.dataset:
+            self.query_type = PlotRequest.MULTI
+            self.x_args = x_args
+            self.y_args = y_args
+        else:
+            if x_args.dataset or y_args.dataset:
                 self.query_type = PlotRequest.SINGLE
-                self.init_single_two_metric()
             else:
-                self.query_type = PlotRequest.MULTI
-                self.init_multi()
-        elif self.y_type == "results" or self.x_type == "results":
-            self.query_type = PlotRequest.SINGLE
-            self.init_single()
-        else:
-            self.query_type = PlotRequest.NONE
-            self.init_none()
-
-        self.plot_args.group_by = args.get("legend_attribute")
-        self.plot_args.group_by_second = args.get("legend_attribute_(second)")
-
-    def init_multi(self):
-        self.plot_args = PlotRequest.PlotArgs(
-            x_attr=self.x_metric,
-            y_attr=self.y_metric,
-            x_dataset=self.x_dataset,
-            y_dataset=self.y_dataset,
-            x_task=self.x_task,
-            y_task=self.y_task,
-        )
-        self.x_args = PlotRequest.DataArgs(
-            dataset=self.x_dataset,
-            task=self.x_task,
-            head=self.x_head,
-            resolution=self.x_resolution,
-        )
-        if self.x_task.name == TaskType.CLASSIFICATION.value:
-            self.x_args.resolution = self.x_resolution
-        self.y_args = PlotRequest.DataArgs(
-            dataset=self.y_dataset,
-            task=self.y_task,
-            head=self.y_head,
-            resolution=self.y_resolution,
-        )
-        if self.y_task.name == TaskType.CLASSIFICATION.value:
-            self.y_args.resolution = self.y_resolution
-
-    def init_single(self):
-        if self.x_type == "results":
-            self.data_args = PlotRequest.DataArgs(
-                dataset=self.x_dataset,
-                task=self.x_task,
-                head=self.x_head,
-                resolution=self.x_resolution,
-                gpu=self.y_gpu,
-                precision=self.y_precision,
-            )
-            self.plot_args = PlotRequest.PlotArgs(
-                x_attr=self.x_metric,
-                y_attr=self.y_type,
-                dataset=self.x_dataset,
-                x_task=self.x_task,
-            )
-        else:
-            self.data_args = PlotRequest.DataArgs(
-                dataset=self.y_dataset,
-                task=self.y_task,
-                head=self.y_head,
-                resolution=self.y_resolution,
-                gpu=self.x_gpu,
-                precision=self.x_precision,
-            )
-            self.plot_args = PlotRequest.PlotArgs(
-                x_attr=self.x_type,
-                y_attr=self.y_metric,
-                dataset=self.y_dataset,
-                y_task=self.y_task,
-            )
-
-    def init_single_two_metric(self):
-        self.data_args = PlotRequest.DataArgs(
-            dataset=self.x_dataset,
-            task=self.x_task,
-            head=self.x_head,
-            resolution=self.x_resolution,
-        )
-        self.plot_args = PlotRequest.PlotArgs(
-            x_attr=self.x_metric,
-            y_attr=self.y_metric,
-            dataset=self.x_dataset,
-            x_task=self.x_task,
-            y_task=self.y_task,
-        )
-
-    def init_none(self):
-        self.data_args = PlotRequest.DataArgs(
-            gpu=self.x_gpu or self.y_gpu,
-            precision=self.x_precision or self.y_precision,
-        )
-        self.plot_args = PlotRequest.PlotArgs(x_attr=self.x_type, y_attr=self.y_type)
+                self.query_type = PlotRequest.NONE
