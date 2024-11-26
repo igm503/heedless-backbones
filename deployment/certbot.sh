0c1554134355914dbfe2df273ae29297a6c0074e
igm503 <igm503@gmail.com>
2024-11-26 15:22:01 -0600

    switched deployment over to submodule

--- deployment/certbot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-sudo apt install python3 python3-venv libaugeas0
-sudo python3 -m venv /opt/certbot/
-sudo /opt/certbot/bin/pip install --upgrade pip
-sudo /opt/certbot/bin/pip install certbot certbot-nginx
-sudo ln -sf /opt/certbot/bin/certbot /usr/bin/certbot
-sudo certbot --nginx
